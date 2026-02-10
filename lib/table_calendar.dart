9d7a3a69d0f273e0a405a1f5fea5e63c11f68ae0
Yahyazawadi <yahyazawadi278@gmail.com>
2026-02-10 21:41:53 +0200

    exported date range

--- lib/table_calendar.dart
+++ lib/table_calendar.dart
@@ -8,3 +8,4 @@ export 'src/customization/header_style.dart';
 export 'src/shared/utils.dart';
 export 'src/table_calendar.dart';
 export 'src/table_calendar_base.dart';
+export 'src/shared/date_range.dart';
